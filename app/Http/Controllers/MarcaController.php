82f5f74e040a3d0b5a2d0e106da4809a65dc79c2
Gustavo G Melo <infogustavomelo@gmail.com>
2022-08-25 13:46:17 -0300

    setup

---
+++ app/Http/Controllers/MarcaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Marca;
+use Illuminate\Http\Request;
+
+class MarcaController extends Controller
+{
+        /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Modelo  $modelo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Marca $modelo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Modelo  $modelo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Marca $modelo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Modelo  $modelo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Marca $modelo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Modelo  $modelo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Marca $modelo)
+    {
+        //
+    }
+}
